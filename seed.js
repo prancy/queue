8d593b0d099fc78c02d4778276b16293b7f6d6bf
nancy nguyen <prancynguyen@gmail.com>
2017-11-27 12:32:04 -0800

    Fix Indentation on New and Show Question Page

--- seed.js
+++ seed.js
@@ -1,39 +1,25 @@
-// First we must require our database
+/ First we must require our database
 require('dotenv').config();
 require('./config/database');
 var User = require('./models/User');
 
-// Import our Mongoose models
-// const Teacher = require('./models/User');
 
 // Destroy all data in database
 const seed = Promise.all([User.remove({})])
 
 
 seed.then(() => {
-    return User.create([
-        { fullName: 'Jim', instructor: true },
-        { fullName: 'Jon', instructor: true },
-        { githubUserName: 'pfinazzo', instructor: false},
-        { githubUserName: 'mchristenson5891', instructor: true, cohort: "53" },
-        { githubUserName: 'anessaa', instructor: true},
-        { githubUserName: 'prancy', instructor: true, quizzes:["5a15f89bd0c2e1f48901393f"]},
-        { githubUserName: 'pfinazzo', instructor: true, quizzes:["5a14ea1f54e047e25859d8e0"]}
-    ])
+   return User.create([
+       { fullName: 'Jim', instructor: true },
+       { fullName: 'Jon', instructor: true },
+       { githubUserName: 'pfinazzo', instructor: true},
+       { githubUserName: 'mchristenson5891', instructor: true, cohort: "44" },
+       { githubUserName: 'anessaa', instructor: false}
+   ])
 })
 
-// Let's log out all students and add add 2 teachers
-// .then((users) => {
-//     console.log(users);
-//     return User.create([
-//         { name: "Jay", age: 34, class: 'UXDI', students: [ students[0]._id, students[1]._id ] },
-//         { name: "Jon", age: 7, class: 'WDI', students: [ students[2]._id, students[3]._id ] },
-//     ])
-// })
-
-// Let's log out teachers and close the connection to our Mongoose database
 .then((users) => {
-    console.log(users);
-    require('mongoose').connection.close();
-    process.exit();
+   console.log(users);
+   require('mongoose').connection.close();
+   process.exit();
 })
